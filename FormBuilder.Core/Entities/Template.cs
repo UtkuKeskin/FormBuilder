397b4dfc1870b53f61f6bfabcfe9bfe3e1a513b4
Utku Keskin <utkukeskin@Utku-MacBook-Pro.local>
2025-07-02 20:42:44 +0300

    add required field toggle for form questions

--- FormBuilder.Core/Entities/Template.cs
+++ FormBuilder.Core/Entities/Template.cs
@@ -25,84 +25,115 @@ namespace FormBuilder.Core.Entities
         public string CustomString1Question { get; set; }
         public string CustomString1Description { get; set; }
         public bool CustomString1ShowInTable { get; set; }
+        public bool CustomString1Required { get; set; }
+
         
         public bool CustomString2State { get; set; }
         public string CustomString2Question { get; set; }
         public string CustomString2Description { get; set; }
         public bool CustomString2ShowInTable { get; set; }
+        public bool CustomString2Required { get; set; }
+
         
         public bool CustomString3State { get; set; }
         public string CustomString3Question { get; set; }
         public string CustomString3Description { get; set; }
         public bool CustomString3ShowInTable { get; set; }
+        public bool CustomString3Required { get; set; }
+
         
         public bool CustomString4State { get; set; }
         public string CustomString4Question { get; set; }
         public string CustomString4Description { get; set; }
         public bool CustomString4ShowInTable { get; set; }
+        public bool CustomString4Required { get; set; }
+
         
         // Text Questions (1-4)
         public bool CustomText1State { get; set; }
         public string CustomText1Question { get; set; }
         public string CustomText1Description { get; set; }
         public bool CustomText1ShowInTable { get; set; }
+        public bool CustomText1Required { get; set; }
+
         
         public bool CustomText2State { get; set; }
         public string CustomText2Question { get; set; }
         public string CustomText2Description { get; set; }
         public bool CustomText2ShowInTable { get; set; }
+        public bool CustomText2Required { get; set; }
+
         
         public bool CustomText3State { get; set; }
         public string CustomText3Question { get; set; }
         public string CustomText3Description { get; set; }
         public bool CustomText3ShowInTable { get; set; }
+        public bool CustomText3Required { get; set; }
+
         
         public bool CustomText4State { get; set; }
         public string CustomText4Question { get; set; }
         public string CustomText4Description { get; set; }
         public bool CustomText4ShowInTable { get; set; }
+        public bool CustomText4Required { get; set; }
+
         
         // Integer Questions (1-4)
         public bool CustomInt1State { get; set; }
         public string CustomInt1Question { get; set; }
         public string CustomInt1Description { get; set; }
         public bool CustomInt1ShowInTable { get; set; }
+        public bool CustomInt1Required { get; set; }
+        
         
         public bool CustomInt2State { get; set; }
         public string CustomInt2Question { get; set; }
         public string CustomInt2Description { get; set; }
         public bool CustomInt2ShowInTable { get; set; }
+        public bool CustomInt2Required { get; set; }
+
         
         public bool CustomInt3State { get; set; }
         public string CustomInt3Question { get; set; }
         public string CustomInt3Description { get; set; }
         public bool CustomInt3ShowInTable { get; set; }
+        public bool CustomInt3Required { get; set; }
         
         public bool CustomInt4State { get; set; }
         public string CustomInt4Question { get; set; }
         public string CustomInt4Description { get; set; }
         public bool CustomInt4ShowInTable { get; set; }
+        public bool CustomInt4Required { get; set; }
+
         
         // Checkbox Questions (1-4)
         public bool CustomCheckbox1State { get; set; }
         public string CustomCheckbox1Question { get; set; }
         public string CustomCheckbox1Description { get; set; }
         public bool CustomCheckbox1ShowInTable { get; set; }
+        public bool CustomCheckbox1Required { get; set; }
+
         
         public bool CustomCheckbox2State { get; set; }
         public string CustomCheckbox2Question { get; set; }
         public string CustomCheckbox2Description { get; set; }
         public bool CustomCheckbox2ShowInTable { get; set; }
+        public bool CustomCheckbox2Required { get; set; }
+
         
         public bool CustomCheckbox3State { get; set; }
         public string CustomCheckbox3Question { get; set; }
         public string CustomCheckbox3Description { get; set; }
         public bool CustomCheckbox3ShowInTable { get; set; }
+        public bool CustomCheckbox3Required { get; set; }
+
         
         public bool CustomCheckbox4State { get; set; }
         public string CustomCheckbox4Question { get; set; }
         public string CustomCheckbox4Description { get; set; }
         public bool CustomCheckbox4ShowInTable { get; set; }
+        public bool CustomCheckbox4Required { get; set; }
+        
         
         // Navigation properties
         public virtual User User { get; set; }
---
+++ FormBuilder.Infrastructure/Migrations/20250702172010_AddRequiredFieldsToQuestions.cs
@@ -0,0 +1,208 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FormBuilder.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRequiredFieldsToQuestions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "CustomCheckbox1Required",
+                table: "Templates",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "CustomCheckbox2Required",
+                table: "Templates",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "CustomCheckbox3Required",
+                table: "Templates",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "CustomCheckbox4Required",
+                table: "Templates",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "CustomInt1Required",
+                table: "Templates",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "CustomInt2Required",
+                table: "Templates",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "CustomInt3Required",
+                table: "Templates",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "CustomInt4Required",
+                table: "Templates",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "CustomString1Required",
+                table: "Templates",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "CustomString2Required",
+                table: "Templates",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "CustomString3Required",
+                table: "Templates",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "CustomString4Required",
+                table: "Templates",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "CustomText1Required",
+                table: "Templates",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "CustomText2Required",
+                table: "Templates",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "CustomText3Required",
+                table: "Templates",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "CustomText4Required",
+                table: "Templates",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "admin-user-id",
+                column: "PasswordHash",
+                value: "AQAAAAIAAYagAAAAEDr2Vsu4PgZrSlwjgU1xSFFsbT7QwYj4uAyFRkU0QnQ2rBwqNtoeZjwJtZTYQuUfUQ==");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustomCheckbox1Required",
+                table: "Templates");
+
+            migrationBuilder.DropColumn(
+                name: "CustomCheckbox2Required",
+                table: "Templates");
+
+            migrationBuilder.DropColumn(
+                name: "CustomCheckbox3Required",
+                table: "Templates");
+
+            migrationBuilder.DropColumn(
+                name: "CustomCheckbox4Required",
+                table: "Templates");
+
+            migrationBuilder.DropColumn(
+                name: "CustomInt1Required",
+                table: "Templates");
+
+            migrationBuilder.DropColumn(
+                name: "CustomInt2Required",
+                table: "Templates");
+
+            migrationBuilder.DropColumn(
+                name: "CustomInt3Required",
+                table: "Templates");
+
+            migrationBuilder.DropColumn(
+                name: "CustomInt4Required",
+                table: "Templates");
+
+            migrationBuilder.DropColumn(
+                name: "CustomString1Required",
+                table: "Templates");
+
+            migrationBuilder.DropColumn(
+                name: "CustomString2Required",
+                table: "Templates");
+
+            migrationBuilder.DropColumn(
+                name: "CustomString3Required",
+                table: "Templates");
+
+            migrationBuilder.DropColumn(
+                name: "CustomString4Required",
+                table: "Templates");
+
+            migrationBuilder.DropColumn(
+                name: "CustomText1Required",
+                table: "Templates");
+
+            migrationBuilder.DropColumn(
+                name: "CustomText2Required",
+                table: "Templates");
+
+            migrationBuilder.DropColumn(
+                name: "CustomText3Required",
+                table: "Templates");
+
+            migrationBuilder.DropColumn(
+                name: "CustomText4Required",
+                table: "Templates");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "admin-user-id",
+                column: "PasswordHash",
+                value: "AQAAAAIAAYagAAAAECsIkzteEzSY+3G/HqGkrqQEDRduM2/yKK082R9Tt5iW4tqG6ctSgJefy/ZY6Ytdmw==");
+        }
+    }
+}
